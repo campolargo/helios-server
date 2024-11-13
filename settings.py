83c31f87c428e05766322504684dd9fa173ace45
Prefeitura Municipal de Campo Largo <analistas@campolargo.pr.gov.br>
2024-11-13 09:11:52 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -63,13 +63,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Los_Angeles'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_TZ = False
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 SITE_ID = 1
 
